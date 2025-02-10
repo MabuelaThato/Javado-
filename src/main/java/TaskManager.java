bc1358d3024f5547d5f25071b5daf8f5e41aadf6
Thato Mabuela <mabuelathato03@gmail.com>
2025-02-10 22:03:08 +0200

    Add code to load tasks on startup

--- src/main/java/TaskManager.java
+++ src/main/java/TaskManager.java
@@ -1,14 +1,27 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 import java.io.IOException;
 
 public class TaskManager {
     private List<String> tasks; // hint: will change in iteration 3
 
     public TaskManager() {
-        // Initialize tasks list
         this.tasks = new ArrayList<String>();
+        try {
+            File file = new File("tasks.txt");
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                this.tasks.add(line);
+            }
+            scanner.close();
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
     }
 
     public void addTask(String task) {
@@ -34,7 +47,7 @@ public class TaskManager {
             myWriter.close();
             System.out.println("Successfully wrote tasks to the file.");
         } catch (IOException e) {
-            System.out.println("An error occurred.");
+            System.out.println("An error occurred while writing tasks to the file.");
         }
     }
 }
